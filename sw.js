1eabc9c51aefb86f2a89ad809b04f022f76a57fc
nikoschr13 <nchristou@me.com>
2026-04-13 18:29:46 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-
-self.addEventListener('install', e => e.waitUntil(caches.open('german-trainer-v2').then(c => c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
